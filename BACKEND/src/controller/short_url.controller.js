952c0b66017b27c296103242fc2a77c94d801613
safalgupta <safal.2201106cs@iiitbh.ac.in>
2025-08-20 21:40:10 +0530

    fix short_url controller bug

--- BACKEND/src/controller/short_url.controller.js
+++ BACKEND/src/controller/short_url.controller.js
@@ -10,7 +10,8 @@ export const createShortUrl = wrapAsync(async (req,res)=>{
     }else{  
         shortUrl = await createShortUrlWithoutUser(data.url)
     }
-    res.status(200).json({shortUrl : process.env.APP_URL + shortUrl})
+    // res.status(200).json({shortUrl : process.env.APP_URL + shortUrl})
+    res.status(200).json({shortUrl : process.env.APP_URL.replace(/\/$/, '') + '/' + shortUrl})
 })
 
 
@@ -24,5 +25,7 @@ export const redirectFromShortUrl = wrapAsync(async (req,res)=>{
 export const createCustomShortUrl = wrapAsync(async (req,res)=>{
     const {url,slug} = req.body
     const shortUrl = await createShortUrlWithoutUser(url,customUrl)
-    res.status(200).json({shortUrl : process.env.APP_URL + shortUrl})
+    // res.status(200).json({shortUrl : process.env.APP_URL + shortUrl})
+    
+    res.status(200).json({shortUrl : process.env.APP_URL.replace(/\/$/, '') + '/' + shortUrl})
 })
